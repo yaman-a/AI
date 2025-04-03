cde4790f380616778537bf0da7b61ac682ad91c9
yaman-a <yaman.ashqar05@gmail.com>
2025-04-03 13:45:52 +1030

    assignment start

---
+++ pathfinder.py
@@ -0,0 +1,15 @@
+STUDENT_ID = 'a1884774'
+DEGREE = 'UG'
+
+def graph_search():
+    print('graph_search')
+
+def main():
+    print('main')
+
+
+if __name__ == "__main__":
+    input = input().split()
+
+
+    
