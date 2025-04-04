50ae365ce26c4b297dfdba2019af00ea6881d5f6
yaman-a <yaman.ashqar05@gmail.com>
2025-04-04 23:40:55 +1030

    buh

--- pathfinder.py
+++ pathfinder.py
@@ -75,44 +75,44 @@ def bfs(graph, start, end, n):
                     q.append((neighbor, new_path))
                     seen.add(neighbor)
 
-def path_cost(from_node, to_node):
-    return 1 + max(0, to_node - from_node)
+def path_cost(from_n, to_n):
+    return 1 + max(0, to_n - from_n)
 
-def ucs(grid, start, goal, n):
+def ucs(graph, start, goal, size):
     count = 0
-    rows, cols = n
+    rows, cols = size
 
     q = []
     pq.heappush(q, (0, count, start))
     count += 1
 
     dest_start = {}
-    curr_cost = {start: 0}
+    cost_so_far = {start: 0}
 
     while q:
-        curr_cost, _, curr = pq.heappop(q)
+        current_cost, _, current = pq.heappop(q)
 
-        if curr == goal:
-            return reconstruct_path(dest_start, curr)
+        if current == goal:
+            return reconstruct_path(dest_start, current)
 
-        i, j = curr
+        i, j = current
         dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         for dx, dy in dirs:
             ni, nj = i + dx, j + dy
             if 0 <= ni < rows and 0 <= nj < cols:
                 neighbor = (ni, nj)
-                if grid[ni][nj] == 'X':
+                if graph[ni][nj] == 'X':
                     continue
 
-                from_e = int(grid[i][j])
-                to_e = int(grid[ni][nj])
+                from_e = int(graph[i][j])
+                to_e = int(graph[ni][nj])
                 step = path_cost(from_e, to_e)
-                new_cost = curr_cost[curr] + step
+                new_cost = cost_so_far[current] + step
 
-                if neighbor not in curr_cost or new_cost < curr_cost[neighbor]:
-                    curr_cost[neighbor] = new_cost
-                    dest_start[neighbor] = curr
+                if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                    cost_so_far[neighbor] = new_cost
+                    dest_start[neighbor] = current
                     pq.heappush(q, (new_cost, count, neighbor))
                     count += 1
 
@@ -125,12 +125,12 @@ def reconstruct_path(dest_start, curr):
         path.append(curr)
     return path[::-1]
     
-def astar(grid, start, goal, n, heuristic):
+def astar(graph, start, end, n, heuristic):
     count = 0
     rows, cols = n
 
     open_set = []
-    pq.heappush(open_set, (heuristic(start, goal), count, start))
+    pq.heappush(open_set, (heuristic(start, end), count, start))
     count += 1
 
     dest_start = {}
@@ -139,7 +139,7 @@ def astar(grid, start, goal, n, heuristic):
     while open_set:
         _, _, curr = pq.heappop(open_set)
 
-        if curr == goal:
+        if curr == end:
             return reconstruct_path(dest_start, curr)
 
         i, j = curr
@@ -149,18 +149,18 @@ def astar(grid, start, goal, n, heuristic):
             ni, nj = i + dx, j + dy
             if 0 <= ni < rows and 0 <= nj < cols:
                 neighbor = (ni, nj)
-                if grid[ni][nj] == 'X':
+                if graph[ni][nj] == 'X':
                     continue
 
-                from_e = int(grid[i][j])
-                to_e = int(grid[ni][nj])
+                from_e = int(graph[i][j])
+                to_e = int(graph[ni][nj])
                 move_cost = path_cost(from_e, to_e)
                 notconfirmed_g = g_score[curr] + move_cost
 
                 if notconfirmed_g < g_score.get(neighbor, float('inf')):
                     dest_start[neighbor] = curr
                     g_score[neighbor] = notconfirmed_g
-                    pq.heappush(open_set, (notconfirmed_g + heuristic(neighbor, goal), count, neighbor))
+                    pq.heappush(open_set, (notconfirmed_g + heuristic(neighbor, end), count, neighbor))
                     count += 1
 
     return None
@@ -178,32 +178,32 @@ def main():
     with open(map_file, 'r') as f:
         lines = f.read().splitlines()
 
-    grid_rows, grid_cols = map(int, lines[0].split())
+    graph_rows, graph_cols = map(int, lines[0].split())
     start_row, start_col = map(int, lines[1].split())
     end_row, end_col = map(int, lines[2].split())
-    grid = [line.split() for line in lines[3:]]
+    graph = [line.split() for line in lines[3:]]
 
     start = (start_row - 1, start_col - 1)
     end = (end_row - 1, end_col - 1)
-    n = (grid_rows, grid_cols)
+    n = (graph_rows, graph_cols)
 
     if algo == 'bfs':
-        path = bfs(grid, start, end, n)
+        path = bfs(graph, start, end, n)
     elif algo == 'ucs':
-        path = ucs(grid, start, end, n)
+        path = ucs(graph, start, end, n)
     elif algo == 'astar':
         if heuristic == 'euclidean':
             h_fn = euclidean
         elif heuristic == 'manhattan':
             h_fn = manhattan
-        path = astar(grid, start, end, n, h_fn)
+        path = astar(graph, start, end, n, h_fn)
 
     if mode == 'debug':
         print("path:")
         if path:
             for i, j in path:
-                grid[i][j] = '*'
-            for row in grid:
+                graph[i][j] = '*'
+            for row in graph:
                 print(" ".join(row))
         else:
             print("null")
@@ -215,8 +215,8 @@ def main():
     elif mode == 'release':
         if path:
             for i, j in path:
-                grid[i][j] = '*'
-            for row in grid:
+                graph[i][j] = '*'
+            for row in graph:
                 print(" ".join(row))
         else:
             print("null")
